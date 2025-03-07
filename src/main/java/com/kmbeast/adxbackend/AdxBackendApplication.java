2208f164bff29c6a78f1a63f2f1a9ed368ade76c
hahaha778 <438643418@qq.com>
2025-03-07 17:00:43 +0800

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/kmbeast/adxbackend/AdxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kmbeast.adxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmbeast/adxbackend/AdxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmbeast.adxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
